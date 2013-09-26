ec38a1557e0e09bab9c391da1f4baba2c681d6d8
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-09-25 21:51:52 -0400

    Group commands in the same list to be able to have ordered flags

--- xm/main.py
+++ xm/main.py
@@ -19,23 +19,23 @@ def _new_argument_parser():
 
     parser.add_argument(
         '-u', '--unittest', help='run unittest',
-        action='store_const', const=True, default=False,
+        action='append_const', dest='commands', const='unittest',
     )
     parser.add_argument(
         '-s', '--sync', help='sync local copy on the server',
-        action='store_const', const=True, default=False,
+        action='append_const', dest='commands', const='sync',
+    )
+    parser.add_argument(
+        '--setup', help='run commands that should be run before sync',
+        action='append_const', dest='commands', const='setup',
+    )
+    parser.add_argument(
+        '-d', '--dep', help='install missing dependencies on the server',
+        action='append_const', dest='commands', const='dep',
     )
     parser.add_argument(
         '-f', '--file', help='specify the configuration file',
         default=DEFAULT_CONFIG_FILE,
-    )
-    parser.add_argument(
-        '--setup', help='run commands that should be run before sync',
-        action='store_const', const=True, default=False,
-    )
-    parser.add_argument(
-        '-d', '--dep', help='install missing dependencies on the server',
-        action='store_const', const=True, default=False,
     )
     parser.add_argument(
         '-t', '--target', help='the target on which to run'
